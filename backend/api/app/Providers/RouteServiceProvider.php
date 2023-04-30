af01ac75055ec9a7c4f2c9094d77bd097831a956
sal0000 <shio.kunisawa@gmail.com>
2023-04-30 15:45:06 +0900

    update backend Changed auth route path to api/auth

--- backend/api/app/Providers/RouteServiceProvider.php
+++ backend/api/app/Providers/RouteServiceProvider.php
@@ -31,11 +31,15 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
-                ->group(base_path('routes/api.php'))
-                ->group(base_path('routes/auth.php'));
+                ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+
+            Route::middleware('api')
+                ->prefix('api/auth')
+                ->group(base_path('routes/auth.php'));
         });
     }
 }
